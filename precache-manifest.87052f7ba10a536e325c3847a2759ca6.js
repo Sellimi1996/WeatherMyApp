a850885d579431f6f9a698f4d5757e3f2565ac8d
Sellimi1996 <younes10sillimi@gmail.com>
2021-02-06 19:10:11 +0100

    Updates

---
+++ precache-manifest.87052f7ba10a536e325c3847a2759ca6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "109e9665790b1011e23870b196e3e385",
+    "url": "/WeatherMyApp/index.html"
+  },
+  {
+    "revision": "303711717338e2c65982",
+    "url": "/WeatherMyApp/static/css/2.dafd5b16.chunk.css"
+  },
+  {
+    "revision": "59de86acecfda9692d0e",
+    "url": "/WeatherMyApp/static/css/main.3501733f.chunk.css"
+  },
+  {
+    "revision": "303711717338e2c65982",
+    "url": "/WeatherMyApp/static/js/2.e0cc8ac5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/WeatherMyApp/static/js/2.e0cc8ac5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59de86acecfda9692d0e",
+    "url": "/WeatherMyApp/static/js/main.5ed778f7.chunk.js"
+  },
+  {
+    "revision": "84d8a3b00de53a7a2819",
+    "url": "/WeatherMyApp/static/js/runtime-main.d0e89b34.js"
+  },
+  {
+    "revision": "e1676501d6a72a7c691a39a600fb98f5",
+    "url": "/WeatherMyApp/static/media/img.e1676501.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/WeatherMyApp/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/WeatherMyApp/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/WeatherMyApp/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/WeatherMyApp/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/WeatherMyApp/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
